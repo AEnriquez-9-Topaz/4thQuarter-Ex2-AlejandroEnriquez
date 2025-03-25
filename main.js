6deddaec599f3ee0a542768d44de4df42284706b
AJ (Alejandro) <enriquez.alm@obmontessori.edu.ph>
2025-03-25 11:16:44 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function verifyAge () {
+
+	// If statement
+
+		// let age = document.getElementById('inputAge').value;
+		// if (age<18 && age>0){window.alert("You are not eligible to vote")};
+		// if (age>17 && age<120) {window.alert("You are eligable to vote")}
+
+	// If-else statement
+	let age = document.getElementById('inputAge').value;
+	if (age<18){window.alert("You are not eligible to vote")}
+	else {window.alert("You are eligable to vote")}
+		}	
+ 
+
+						
